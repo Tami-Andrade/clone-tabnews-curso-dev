e5e3ee6812e7cbd1e27c910a45dc0d41d1d122da
Tami-Andrade <107888160+Tami-Andrade@users.noreply.github.com>
2024-11-22 01:29:28 +0000

    chore: add dog photo

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home(){
     return (
         <>
         <h1>Meu bem, olha que foto fofa tirei da Pandora hoje!</h1>
+        <img src="/sources/IMG_20241013_131037.jpg" alt="Pandora"/>
         </>
 
     );
