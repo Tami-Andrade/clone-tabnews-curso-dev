db07be42eabcaa39bac7d1f44d04cf17cd959adb
Tami-Andrade <107888160+Tami-Andrade@users.noreply.github.com>
2024-11-29 01:08:29 +0000

    style: add scripts `lint:check` and `lint:fix``

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-function Home(){
-    return (
-        <>
-        <h1>Meu bem, olha que foto fofa tirei da Pandora hoje!</h1>
-        <img src="/sources/IMG_20241013_131037.jpg" alt="Pandora"/>
-        </>
-
-    );
+function Home() {
+  return (
+    <>
+      <h1>Meu bem, olha que foto fofa tirei da Pandora hoje!</h1>
+      <img src="/sources/IMG_20241013_131037.jpg" alt="Pandora" />
+    </>
+  );
 }
 
 export default Home;
