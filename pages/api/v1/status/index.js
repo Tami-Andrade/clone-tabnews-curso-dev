de831c1bc4fa9977612e9795a7af9376143c2dfb
Tami-Andrade <107888160+Tami-Andrade@users.noreply.github.com>
2025-01-24 01:51:53 +0000

    build: adjustments

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('select 1+3 AS SOMA')
-  console.log(result)
+  const result = await database.query("select 1+3 AS SOMA");
+  console.log(result);
   response.status(200).json({ texto: "testando" });
 }
 export default status;
