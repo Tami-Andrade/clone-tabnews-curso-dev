8054fbced8a3ff3d0035508c3ad341823b7f6330
Tami-Andrade <107888160+Tami-Andrade@users.noreply.github.com>
2025-01-29 00:06:32 +0000

    tests: add database connection test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,22 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+33 AS SOMA");
-  console.log(result);
-  response.status(200).json({ texto: "testando" });
+  const updatedAt = new Date().toISOString();
+  const dbVersion = await database.query("SHOW server_version;");
+
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+
+  const dbAliveConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active'",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: dbVersion.rows[0].server_version,
+      max_connections: parseInt(dbMaxConnections.rows[0].max_connections),
+      alive_connections: parseInt(dbAliveConnections.rows[0].count),
+    },
+  });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parseUpdatedAt);
+
+  expect(responseBody.database.version).not.toBe(null);
+  expect(responseBody.database.alive_connections).toBeGreaterThan(0);
+
+  console.log(responseBody);
 });
