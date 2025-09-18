244ad7df4cc3d4ac3d722770dc1df961d8b2f041
Tami-Andrade <107888160+Tami-Andrade@users.noreply.github.com>
2025-09-18 12:51:27 +0000

    testes

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,14 +1,34 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path"
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
+
+  if (request.method === "GET") {
+    console.log("GET method");
+    const migrations = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join("infra","migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations"
-  });
-  response.status(200).json(migrations);
+    });
+    response.status(200).json(migrations);
+  }
+  if (request.method === "POST") {
+    console.log("POST method");
+    const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: false,
+    dir: join("infra","migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations"
+    });
+    response.status(200).json(migrations);
+  }
+
+  response.status(405).json({message: "Method not allowed"});
+
+
 }
 
